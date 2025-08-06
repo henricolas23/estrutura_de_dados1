81beef3588b97750ba2f628e3b90ec060e2613bf
henricolas23 <henricolas23filizola@gmail.com>
2025-08-05 23:10:43 -0300

    add

--- exemplo001.c
+++ exemplo001.c
@@ -1,1 +1,17 @@
-ad
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Definição do TAD fração 
+
+typedef struct {
+    int numerrador;
+    int denominador;
+}fracao;
+
+int main (){
+
+    int t = sizeof(fracao);
+    printf("tamanho : %d\n", t);
+    
+}
---
+++ exemplo002.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct pessoa {
+    char Nome[150]; // gastar 150 + 1 caracteres 
+    char Telefone[14]; // 15 caracteres 
+    char endereco[200]; //201 caracteres
+} pessoa;
+
+int mian() {
+typedef int inteiro
+
+
+strcpy(X.Nome, "Maria Alves");
+strcpy(X.Telefone, "(61) 12345-6789");
+strcpy(X.endereco, "SHIS 12 25 13");
+
+printf("%s\t%s\t%s\n", X.Nome, X.endereco, X.Telefone);
+

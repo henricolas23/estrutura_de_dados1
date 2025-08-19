a08586af7ae41af8f734fb76fc4151bf7c5a778d
henricolas23 <henricolas23filizola@gmail.com>
2025-08-19 11:26:14 -0300

    add

---
+++ aula_02/exemplo_01.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    FILE * fp; // fp é um ponteiro para arquivo 
+    printf("%d %x\n", fp, fp); // mostrar o endereço do arquivo
+    return 0;
+}
---
+++ teste.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    FILE * fp; // fp é um ponteiro para arquivo 
+    fp = fopen("teste.txt","w"); // abre teste.txt
+    
